e44517da8645a58bc0d81250239aa2f53a3c450e
Joseph Zhou <josephzh23@hotmail.com>
2023-01-10 21:23:58 -0800

    dd

--- untitled/src/Search/binary_search_loop.kt
+++ untitled/src/Search/binary_search_loop.kt
@@ -1,17 +1,17 @@
 package Search
 
 fun binarySearch(
-        sortedArray: IntArray, target: Int, start: Int, end: Int): Int {
+        arr: IntArray, target: Int, start: Int, end: Int): Int {
     var low = start
     var high = end
     var index = Int.MAX_VALUE
     while (low <= high) {
         val mid = low + (high - low) / 2
-        if (sortedArray[mid] < target) {
+        if (arr[mid] < target) {
             low = mid + 1
-        } else if (sortedArray[mid] > target) {
+        } else if (arr[mid] > target) {
             high = mid - 1
-        } else if (sortedArray[mid] == target) {
+        } else if (arr[mid] == target) {
             index = mid
             break
         }
